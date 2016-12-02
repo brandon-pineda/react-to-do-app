8f1477554f1ed35b7205e25304765cd8ccb48c2b
Brandon Pineda <bppsparton@gmail.com>
2016-12-01 20:19:47 -0800

    error signaling now added for blank or duplicate entries

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    constructor(){
+        super()
+        this.state = {error: null}
+    }
+
     submitted = (e) => {
         e.preventDefault()
-        this.props.submitted(this.refs.createInput.value)
+        let input = this.refs.createInput.value
+        let validation = this.validateInput(input)
+
+        if (validation) {
+            this.setState({ error: validation })
+            return
+        }
+
+        this.setState({ error: null });
+        this.props.submitted(input)
         this.refs.createInput.value = ''
     }
     render() {
@@ -11,8 +25,30 @@ class Form extends Component {
             <form onSubmit={this.submitted}>
                 <input type="text" placeholder="what do I need to do?" ref="createInput"/>
                 <input type="submit"/>
+                {this.renderError()}
             </form>
         )
+    }
+
+    renderError() {
+        if (!this.state.error){
+            return null;
+        }
+        return (
+            <div style={{ color: 'red' }}>
+                {this.state.error}
+            </div>
+        )
+    }
+
+    validateInput = (input) => {
+        if (!input) {
+            return 'Cannot enter a blank task!'
+        } else if (this.props.todos.includes(input)) {
+            return 'Task already exists!'
+        } else {
+            return null;
+        }
     }
 }
 
